84b8772c6475a9a24c03871a6c2a21da50c57ac4
silabantu <112931609+silabantu@users.noreply.github.com>
2022-11-14 13:19:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,9 +18,9 @@ global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','6285731811079','6285339388512']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['994406362810']
+global.premium = ['6289509158681']
+global.packname = 'The Jon Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
